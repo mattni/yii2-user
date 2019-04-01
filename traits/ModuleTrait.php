23387f5342058887928bfeca1e1a978767b0c4f4
mattni <mattieevert@gmail.com>
2019-04-01 16:21:26 +0300

    typo

--- traits/ModuleTrait.php
+++ traits/ModuleTrait.php
@@ -29,5 +29,4 @@ trait ModuleTrait
         return \Yii::$app->getModule('user') ? \Yii::$app->getModule('user')->getDb() : \Yii::$app->db;
         //info: https://github.com/dektrium/yii2-user/commit/4b06f2b057b488091ae91f77e749890e189c7376
     }
-    }
 }
